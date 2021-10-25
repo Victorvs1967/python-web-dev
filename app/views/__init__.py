d527acf23539f88000f14f16f2ff1dcb00c3d5f1
Victor <victorsmirnov@me.com>
2021-10-25 18:17:02 +0300

    any edit

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,16 +3,14 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__, template_folder='tem[lates')
 
-
 @views.route('/')
 def hello_world():
-  return render_template('index.html')
-
+  name = 'Victor'
+  return render_template('index.html', name=name)
 
 @views.route('/menu')
 def menu():
   return render_template('menu.html')
-
 
 @views.route('/login')
 def login():
